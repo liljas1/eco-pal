c2d235ce2422f4b3cd3b0845fd422539e71bb686
Lovro Iljas <lovro.iljas@gmail.com>
2020-11-02 15:42:48 +0100

    Added prompt

--- src/components/EcoPal.vue
+++ src/components/EcoPal.vue
@@ -2,7 +2,7 @@
     <div>
         <b-navbar type="is-dark" fixed-top>
             <template slot="start">
-                <b-navbar-item href="#">
+                <b-navbar-item @click="prompt">
                     Upload a location
                 </b-navbar-item>
                 <b-navbar-item href="#">
@@ -20,16 +20,47 @@
         </b-navbar>
 
         <GmapMap
-            :center="{lat:5, lng:5}"
+            :center="coordinates"
             :zoom="7"
-            style="width: 1080px; height: 720px; margin: 20px auto"
+            style="width: 80%; height: 720px; margin: 0 auto"
             >
         </GmapMap>
     </div>
 </template>
 
 <script>
-    export default {}
+    export default {
+        methods:{
+            prompt() {
+                this.$buefy.dialog.prompt({
+                    message: 'Enter an adress:',
+                    inputAttrs: {
+                        placeholder: 'e.g. Vrbik 8, Zagreb, Croatia',
+                        maxlength: 50
+                    },
+                    trapFocus: true,
+                    onConfirm: (value) => this.$buefy.toast.open(`The location has been sent to the local utility service: ${value}`)
+                })
+            }
+        },
+
+        data (){
+            return{
+                coordinates: {
+                    lat: 0,
+                    lng: 0
+                }
+            }
+        },
+
+        created (){
+            this.$getLocation({})
+                .then(coordinates =>{
+                    this.coordinates = coordinates;
+                })
+                .catch(error => alert(error));
+        }
+    }
 </script>
 
 <style scoped>
